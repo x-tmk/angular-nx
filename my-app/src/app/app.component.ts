30454e9f1083eca94c13ef53a7f24b64d3db9cb9
tomasz.laskowski <tomasz.laskowski@xebia.com>
2024-10-31 15:39:59 +0100

    updating UI

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgForOf, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgForOf, UpperCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Volotea';
-
-  constructor(private router: Router) {
-  }
-
-  navigateTo(link: string) {
-    this.router.navigate([link])
-  }
+  modules = ['search', 'booking', 'change', 'checkin', 'profile'];
 }
