2942076b08cb3c30b8e8087f52caaa8b97f1ecd0
tomasz.laskowski <tomasz.laskowski@xebia.com>
2024-11-05 09:05:19 +0100

    removing dependencies

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgForOf } from '@angular/common';
 import { HeaderComponent } from './components/common/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
